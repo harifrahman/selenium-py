bbe2f560dafa2c41f93347669cb4e0aa25f8aaac
Arif Hakim <harifrahman999@gmail.com>
2025-03-11 17:52:59 +0700

    feat: selenium navigator for adding high price item to cart

--- app.py
+++ app.py
@@ -20,5 +20,21 @@ driver.find_element(By.ID, "password").send_keys("secret_sauce")
 driver.find_element(By.XPATH, '//*[@id="login-button"]').click()
 sleep(3)
 
+# click select-option filter
+driver.find_element(By.CLASS_NAME, "product_sort_container").click()
+driver.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/div/span/select/option[4]').click()
+
+# find all button (add-to-cart)
+buttons_add_to_cart = driver.find_elements(By.CSS_SELECTOR, '.btn.btn_primary.btn_small.btn_inventory')
+
+# get max 3 item or less
+item_to_add_to_cart = 3 if len(buttons_add_to_cart) > 3 else len(buttons_add_to_cart)
+for i in range(item_to_add_to_cart):
+  buttons_add_to_cart[i].click()
+
+# check if item added to cart succesfully
+driver.find_element(By.ID, "shopping_cart_container").click()
+sleep(5)
+
 # terminate chrome driver
 driver.quit()
